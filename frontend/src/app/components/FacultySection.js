99f8bffa199ff08d10f216e3fc8ecba66de324c3
rajatbhardwaj1 <rb9878203799@gmail.com>
2025-03-07 01:13:00 +0530

    facutly added

--- frontend/src/app/components/FacultySection.js
+++ frontend/src/app/components/FacultySection.js
@@ -13,7 +13,7 @@ const FacultySection = () => {
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-12 items-start">
         {/* Faculty Member 1 */}
         <div className="animate__animated animate__fadeInUp bg-gray-200 p-8 rounded-lg shadow-lg text-center">
-          <h3 className="text-2xl font-bold text-black mb-4">Er. Kumar Bhart</h3>
+          <h3 className="text-2xl font-bold text-black mb-4">Er. Kumar Bharti</h3>
           <p className="text-gray-700 font-semibold">Physics Educator</p>
           <p className="text-gray-700">NEET | JEE | 11th | 12th (Phy)</p>
           <p className="text-gray-700">9th | 10th Science</p>
